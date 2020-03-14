8c82d7cca6930f141594135d3b50b2cffc9ca07b
HamzaJadid <hamzah.jadeed@gmail.com>
2020-03-14 13:00:39 +0200

    extracting authorization from the headers

--- routes/api/areeba.js
+++ routes/api/areeba.js
@@ -8,13 +8,13 @@ const router = express.Router();
  * @access Public
  */
 router.post("/oauth2/token", (req, res) => {
+  console.log(req.headers);
   const options = {
     method: "POST",
     url: "https://api.areeba.com/oauth2/token",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
-      Authorization:
-        "Basic TjJndmVGTUxJTjhEa1I2VVNHZVZtdUV2d2NFYTpQaDRla3pJUmxGWmdJOFVqalFLQnhyc1Y1NW9h"
+      Authorization: req.headers.authorization
     },
     form: {
       grant_type: req.body.grant_type
